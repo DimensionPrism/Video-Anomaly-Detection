e834e659d1a425e42f67e90cf2e46f32ea8978bc
Fengwei Liu <liuf7@rpi.edu>
2023-02-18 03:36:14 -0500

    init reproduction

---
+++ models/Accurate-Interpretable-VAD/models/models.py
@@ -0,0 +1,17 @@
+import torch
+import clip
+import numpy as np
+import torch.nn as nn
+
+
+class CLIP(nn.Module):
+    def __init__(self, device) -> None:
+        super(CLIP, self).__init__()
+
+        self.model_, = clip.load("ViT-B/16", device=device)
+        print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in self.model.parameters()]):,}")
+        print("Context length:", self.model.context_length)
+        print("Vocab size:", self.model.vocab_size)
+
+    def forward(self, x):
+        return self.model.encode_image(x)
