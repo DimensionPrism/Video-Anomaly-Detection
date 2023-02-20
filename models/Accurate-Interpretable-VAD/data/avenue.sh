fc7a45f80d0b137d8a2ff7749143252b58fe6671
Fengwei Liu <liuf7@rpi.edu>
2023-02-20 00:19:11 -0500

    output unzip info

--- models/Accurate-Interpretable-VAD/data/avenue.sh
+++ models/Accurate-Interpretable-VAD/data/avenue.sh
@@ -5,7 +5,7 @@ echo "Downloading CUHK-Avenue dataset....."
 cd ./data
 
 wget "http://101.32.75.151:8181/dataset/avenue.tar.gz"
-tar -xf avenue.tar.gz
+tar -xvf avenue.tar.gz
 rm avenue.tar.gz
 rm avenue/avenue.mat
 cp avenue.mat avenue/
--- models/Accurate-Interpretable-VAD/data/shanghaitech.sh
+++ models/Accurate-Interpretable-VAD/data/shanghaitech.sh
@@ -5,7 +5,7 @@ echo "Downloading ShanghaiTech dataset....."
 cd ./data
 
 wget "http://101.32.75.151:8181/dataset/shanghaitech.tar.gz"
-tar -xf shanghaitech.tar.gz
+tar -xvf shanghaitech.tar.gz
 rm shanghaitech.tar.gz
 
 echo "Download ShanghaiTech successfully..."
