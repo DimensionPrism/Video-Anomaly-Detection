3bf9e5fe0af073f0431c659351e448ab260d5cd8
Fengwei Liu <liuf7@rpi.edu>
2023-02-21 00:07:46 -0500

    fix bug in models.py [AIVAD]

--- models/Accurate-Interpretable-VAD/models/models.py
+++ models/Accurate-Interpretable-VAD/models/models.py
@@ -8,7 +8,7 @@ class CLIP(nn.Module):
     def __init__(self, device) -> None:
         super(CLIP, self).__init__()
 
-        self.model_, = clip.load("ViT-B/16", device=device)
+        self.model, _= clip.load("ViT-B/16", device=device)
         print("Model parameters:", f"{np.sum([int(np.prod(p.shape)) for p in self.model.parameters()]):,}")
         print("Context length:", self.model.context_length)
         print("Vocab size:", self.model.vocab_size)
