709f449f5224a044818bfdcc6f1a709a75cb8a27
Fengwei Liu <liuf7@rpi.edu>
2023-02-26 02:12:49 -0500

    avoid duplicate processing of object detection [AIVAD]

--- models/Accurate-Interpretable-VAD/utils/preprocess_utils/object_detection_utils.py
+++ models/Accurate-Interpretable-VAD/utils/preprocess_utils/object_detection_utils.py
@@ -4,6 +4,7 @@ https://github.com/LiUzHiAn/hf2vad/blob/master/pre_process/extract_bboxes.py
 """
 import os
 import sys
+import gdown
 import subprocess
 
 from tqdm import tqdm
@@ -34,77 +35,93 @@ def download_pretrained():
         print("ResNet50-FPN downloaded!")
     else:
         print("pretrained ResNet50-FPN exists!")
+    
+    print("downloading FlowNet2 checkpoint...")
+    if not os.path.exists("model/pre_trained/FlowNet2_checkpoint.pth.tar"):
+        url = "https://drive.google.com/file/d/1hF8vS6YeHkx3j2pfCeQqqZGwA_PJq_Da/view?usp=sharing"
+        output_path = "model/pre_trained/FlowNet2_checkpoint.pth.tar"
+        gdown.download(url, output_path, quiet=True, fuzzy=True)
+        print("FlowNet2 checkpoint downloaded!")
+    else:
+        print("FlowNet2 checkpoint exists!")
 
 def extract_bboxes(dataset_name, dataset_root):
     # extract bboxes for train data
-    dataset = VideoDataset(dataset_name=dataset_name, root=dataset_root, train=True, sequence_length=1,
-                           bboxes_extractions=True)
-    MIN_AREA = DATASET_CFGS[dataset_name]["min_area"]
-    COVER_THRESHOLD = DATASET_CFGS[dataset_name]["cover_threshold"]
-    area_threshold = DATASET_CFGS[dataset_name]["contour_min_area"]
-    binary_threshold = DATASET_CFGS[dataset_name]["binary_threshold"]
-    gauss_mask_size = DATASET_CFGS[dataset_name]["gauss_mask_size"]
-    confidence_threshold = DATASET_CFGS[dataset_name]['confidence_threshold']
-    predictor = Predictor(confidence_threshold=confidence_threshold)
-    all_bboxes = []
-    all_classes = []
-    for idx in tqdm(range(len(dataset)), total=len(dataset)):
-        batch, _ = dataset.__getitem__(idx)
-        # main frame
-        cur_img = img_tensor2numpy(batch[1])
-        obj_bboxes, bbox_areas, classes = get_objects_bboxes(cur_img, predictor, MIN_AREA, dataset_name)
+    bboxes_train_output_path = os.path.join(os.path.join(dataset_root, dataset_name), '%s_bboxes_train.npy' % dataset_name)
+    bboxes_train_classes_output_path = os.path.join(os.path.join(dataset_root, dataset_name), '%s_bboxes_train_classes.npy' % dataset_name)
+    if not os.path.exists(bboxes_train_output_path) or not os.path.exists(bboxes_train_classes_output_path):
+        dataset = VideoDataset(dataset_name=dataset_name, root=dataset_root, train=True, sequence_length=1,
+                            bboxes_extractions=True)
+        MIN_AREA = DATASET_CFGS[dataset_name]["min_area"]
+        COVER_THRESHOLD = DATASET_CFGS[dataset_name]["cover_threshold"]
+        area_threshold = DATASET_CFGS[dataset_name]["contour_min_area"]
+        binary_threshold = DATASET_CFGS[dataset_name]["binary_threshold"]
+        gauss_mask_size = DATASET_CFGS[dataset_name]["gauss_mask_size"]
+        confidence_threshold = DATASET_CFGS[dataset_name]['confidence_threshold']
+        predictor = Predictor(confidence_threshold=confidence_threshold)
+        all_bboxes = []
+        all_classes = []
+        for idx in tqdm(range(len(dataset)), total=len(dataset)):
+            batch, _ = dataset.__getitem__(idx)
+            # main frame
+            cur_img = img_tensor2numpy(batch[1])
+            obj_bboxes, bbox_areas, classes = get_objects_bboxes(cur_img, predictor, MIN_AREA, dataset_name)
 
-        # filter some overlapped bbox
-        obj_bboxes_after_overlap_removal, classes_after_removal = delete_overlapped_bboxes(obj_bboxes, bbox_areas, COVER_THRESHOLD, classes)
+            # filter some overlapped bbox
+            obj_bboxes_after_overlap_removal, classes_after_removal = delete_overlapped_bboxes(obj_bboxes, bbox_areas, COVER_THRESHOLD, classes)
 
-        foreground_bboxes = get_foreground_bboxes(img_batch_tensor2numpy(batch), obj_bboxes_after_overlap_removal,
-                                                  area_threshold, binary_threshold, gauss_mask_size)
-        if foreground_bboxes.shape[0] > 0:
-            cur_bboxes = np.concatenate((obj_bboxes_after_overlap_removal, foreground_bboxes), axis=0)
-            cur_classes = np.concatenate((classes_after_removal, (np.zeros(len(foreground_bboxes)))), axis=0)
-        else:
-            cur_bboxes = obj_bboxes_after_overlap_removal
-            cur_classes = classes_after_removal
-        all_bboxes.append(cur_bboxes)
-        all_classes.append(cur_classes)
-    np.save(os.path.join(os.path.join(dataset_root, dataset_name),
-                            '%s_bboxes_train.npy' % dataset_name), all_bboxes)
-    np.save(os.path.join(os.path.join(dataset_root, dataset_name),
-                            '%s_bboxes_train_classes.npy' % dataset_name), all_classes)
+            foreground_bboxes = get_foreground_bboxes(img_batch_tensor2numpy(batch), obj_bboxes_after_overlap_removal,
+                                                    area_threshold, binary_threshold, gauss_mask_size)
+            if foreground_bboxes.shape[0] > 0:
+                cur_bboxes = np.concatenate((obj_bboxes_after_overlap_removal, foreground_bboxes), axis=0)
+                cur_classes = np.concatenate((classes_after_removal, (np.zeros(len(foreground_bboxes)))), axis=0)
+            else:
+                cur_bboxes = obj_bboxes_after_overlap_removal
+                cur_classes = classes_after_removal
+            all_bboxes.append(cur_bboxes)
+            all_classes.append(cur_classes)
+        np.save(bboxes_train_output_path, all_bboxes)
+        np.save(bboxes_train_classes_output_path, all_classes)
+        print('bboxes_train saved!')
+    else:
+        print('bboxes_train exists!')
 
     # extract bboxes for test data
-    dataset = VideoDataset(dataset_name=dataset_name, root=dataset_root, train=False, sequence_length=1,
-                           bboxes_extractions=True)
-    MIN_AREA = DATASET_CFGS[dataset_name]["min_area"]
-    COVER_THRESHOLD = DATASET_CFGS[dataset_name]["cover_threshold"]
-    area_threshold = DATASET_CFGS[dataset_name]["contour_min_area"]
-    binary_threshold = DATASET_CFGS[dataset_name]["binary_threshold"]
-    gauss_mask_size = DATASET_CFGS[dataset_name]["gauss_mask_size"]
-    confidence_threshold = DATASET_CFGS[dataset_name]['confidence_threshold']
-    predictor = Predictor(confidence_threshold=confidence_threshold)
-    all_bboxes = []
-    all_classes = []
-    for idx in tqdm(range(len(dataset)), total=len(dataset)):
-        batch, _ = dataset.__getitem__(idx)
-        # main frame
-        cur_img = img_tensor2numpy(batch[1])
-        obj_bboxes, bbox_areas, classes = get_objects_bboxes(cur_img, predictor, MIN_AREA, dataset_name)
+    bboxes_test_output_path = os.path.join(os.path.join(dataset_root, dataset_name), '%s_bboxes_test.npy' % dataset_name)
+    bboxes_test_classes_output_path = os.path.join(os.path.join(dataset_root, dataset_name), '%s_bboxes_test_classes.npy' % dataset_name)
+    if not os.path.exists(bboxes_test_output_path) or not os.path.exists(bboxes_test_classes_output_path):
+        dataset = VideoDataset(dataset_name=dataset_name, root=dataset_root, train=False, sequence_length=1,
+                            bboxes_extractions=True)
+        MIN_AREA = DATASET_CFGS[dataset_name]["min_area"]
+        COVER_THRESHOLD = DATASET_CFGS[dataset_name]["cover_threshold"]
+        area_threshold = DATASET_CFGS[dataset_name]["contour_min_area"]
+        binary_threshold = DATASET_CFGS[dataset_name]["binary_threshold"]
+        gauss_mask_size = DATASET_CFGS[dataset_name]["gauss_mask_size"]
+        confidence_threshold = DATASET_CFGS[dataset_name]['confidence_threshold']
+        predictor = Predictor(confidence_threshold=confidence_threshold)
+        all_bboxes = []
+        all_classes = []
+        for idx in tqdm(range(len(dataset)), total=len(dataset)):
+            batch, _ = dataset.__getitem__(idx)
+            # main frame
+            cur_img = img_tensor2numpy(batch[1])
+            obj_bboxes, bbox_areas, classes = get_objects_bboxes(cur_img, predictor, MIN_AREA, dataset_name)
 
-        # filter some overlapped bbox
-        obj_bboxes_after_overlap_removal, classes_after_removal = delete_overlapped_bboxes(obj_bboxes, bbox_areas, COVER_THRESHOLD, classes)
+            # filter some overlapped bbox
+            obj_bboxes_after_overlap_removal, classes_after_removal = delete_overlapped_bboxes(obj_bboxes, bbox_areas, COVER_THRESHOLD, classes)
 
-        foreground_bboxes = get_foreground_bboxes(img_batch_tensor2numpy(batch), obj_bboxes_after_overlap_removal,
-                                                  area_threshold, binary_threshold, gauss_mask_size)
-        if foreground_bboxes.shape[0] > 0:
-            cur_bboxes = np.concatenate((obj_bboxes_after_overlap_removal, foreground_bboxes), axis=0)
-            cur_classes = np.concatenate((classes_after_removal, (np.zeros(len(foreground_bboxes)))), axis=0)
-        else:
-            cur_bboxes = obj_bboxes_after_overlap_removal
-            cur_classes = classes_after_removal
-        all_bboxes.append(cur_bboxes)
-        all_classes.append(cur_classes)
-    np.save(os.path.join(os.path.join(dataset_root, dataset_name),
-                            '%s_bboxes_test.npy' % dataset_name), all_bboxes)
-    np.save(os.path.join(os.path.join(dataset_root, dataset_name),
-                            '%s_bboxes_test_classes.npy' % dataset_name), all_classes)
-    print('bboxes saved!')
+            foreground_bboxes = get_foreground_bboxes(img_batch_tensor2numpy(batch), obj_bboxes_after_overlap_removal,
+                                                    area_threshold, binary_threshold, gauss_mask_size)
+            if foreground_bboxes.shape[0] > 0:
+                cur_bboxes = np.concatenate((obj_bboxes_after_overlap_removal, foreground_bboxes), axis=0)
+                cur_classes = np.concatenate((classes_after_removal, (np.zeros(len(foreground_bboxes)))), axis=0)
+            else:
+                cur_bboxes = obj_bboxes_after_overlap_removal
+                cur_classes = classes_after_removal
+            all_bboxes.append(cur_bboxes)
+            all_classes.append(cur_classes)
+        np.save(bboxes_test_output_path, all_bboxes)
+        np.save(bboxes_test_classes_output_path, all_classes)
+        print('bboxes_test saved!')
+    else:
+        print('bboxes_test exists!')
