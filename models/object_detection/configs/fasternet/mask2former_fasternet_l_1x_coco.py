37905757710899844b1062860459b00f034a53ce
Fengwei Liu <liuf7@rpi.edu>
2023-03-19 17:42:02 -0400

    new det model

---
+++ models/object_detection/configs/fasternet/mask2former_fasternet_l_1x_coco.py
@@ -0,0 +1,20 @@
+_base_ = [
+    '../_base_/models/mask2former_r50_lsj_8x2_50e_coco-panoptic.py',
+    '../common/mstrain_3x_coco_panoptic.py'
+]
+
+# optimizer
+model = dict(
+    backbone=dict(
+        type='fasternet_l',
+        style='pytorch',
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint='../model_ckpt/fasternet_l-epoch=299-val_acc1=83.5060.pth',
+            ),
+        # init_cfg=None,
+        ),
+)
+# optimizer
+optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, weight_decay=0.0001)
+optimizer_config = dict(grad_clip=None)
