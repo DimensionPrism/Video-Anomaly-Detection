761036e4b40b47ae373e9c2058fea51767e93938
Fengwei Liu <liuf7@rpi.edu>
2023-03-23 11:42:42 -0400

    update coco panoptic dataset config

--- models/object_detection/configs/_base_/datasets/coco_panoptic.py
+++ models/object_detection/configs/_base_/datasets/coco_panoptic.py
@@ -1,21 +1,33 @@
 # dataset settings
 dataset_type = 'CocoPanopticDataset'
 data_root = 'data/coco/'
+image_size = (1024, 1024)
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile', to_float32=True),
     dict(
         type='LoadPanopticAnnotations',
         with_bbox=True,
         with_mask=True,
         with_seg=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
+    # large scale jittering
+    dict(
+        type='Resize',
+        img_scale=image_size,
+        ratio_range=(0.1, 2.0),
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(
+        type='RandomCrop',
+        crop_size=image_size,
+        crop_type='absolute',
+        recompute_bbox=True,
+        allow_negative_crop=True),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='SegRescale', scale_factor=1 / 4),
-    dict(type='DefaultFormatBundle'),
+    dict(type='Pad', size=image_size),
+    dict(type='DefaultFormatBundle', img_to_float=True),
     dict(
         type='Collect',
         keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks', 'gt_semantic_seg']),
